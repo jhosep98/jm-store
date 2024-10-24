8b6ad48417a62c5786562210b18b2de5ec19ffb0
jhosep98 <jhosep149@hotmail.com>
2024-10-23 23:01:57 -0300

    feat(back): home content type

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,23 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text;
+    metaTitle: Schema.Attribute.String;
+    shareImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.seo': SharedSeo;
+    }
+  }
+}
