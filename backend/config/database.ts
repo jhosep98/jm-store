82118538ea4b98042e4517c0b57ecf0ecb71d879
jhosep98 <jhosep149@hotmail.com>
2024-12-23 10:48:19 -0300

    fix(backend): useRouter error

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
+  const client = env('DATABASE_CLIENT');
 
   const connections = {
     mysql: {
@@ -30,15 +30,20 @@ export default ({ env }) => {
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        // ssl: env.bool('DATABASE_SSL'),
+        // ssl: env.bool('DATABASE_SSL', false) && {
+        //   key: env('DATABASE_SSL_KEY', undefined),
+        //   cert: env('DATABASE_SSL_CERT', undefined),
+        //   ca: env('DATABASE_SSL_CA', undefined),
+        //   capath: env('DATABASE_SSL_CAPATH', undefined),
+        //   cipher: env('DATABASE_SSL_CIPHER', undefined),
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        // },
+        ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // Set to false to avoid issues with self-signed certificates
+      },
         schema: env('DATABASE_SCHEMA', 'public'),
+        debug: true
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
